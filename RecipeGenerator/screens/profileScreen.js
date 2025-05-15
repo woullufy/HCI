4fc7116e3636e6991e07667761ab307c94eea689
fruhwirth-b <122296980+fruhwirth-b@users.noreply.github.com>
2025-05-16 00:54:48 +0200

    Update profileScreen.js

--- RecipeGenerator/screens/profileScreen.js
+++ RecipeGenerator/screens/profileScreen.js
@@ -1,10 +1,151 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView } from 'react-native';
+import { View, Text, Switch, StyleSheet } from 'react-native';
+import { COLORS, FONTS, SPACING, RADIUS } from './theme';
+import { MaterialIcons } from '@expo/vector-icons';
 
-export default function ProfileScreen() {
+const ProfileScreen = () => {
+  const [isVegetarian, setisVegetarian] = useState(false);
+  const [isVegan, setisVegan] = useState(false);
+  const [isLactosefree, setisLactosefree] = useState(false);
+  const [isGlutenfree, setisGlutenfree] = useState(false);
+  const [notificationsEnabled, setNotificationsEnabled] = useState(false);
+  const [user1, setuser1] = useState(false);
+  const [user2, setuser2] = useState(false);
+  const [user3, setuser3] = useState(false);
+
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile Screen</Text>
-    </View>
+  <ScrollView style={styles.container}>
+    <View style={styles.container}>
+      <View style={styles.headingContainer}>
+        <MaterialIcons name="settings" size={22} color="black" style={{ marginRight: 4 }} />
+        <Text style={FONTS.heading}>Einstellungen</Text>
+      </View>
+      <View style={styles.divider} />
+      <Text style={FONTS.subheading}>Lebensmitteleinschränkungen</Text>
+      <View style={styles.switchContainer}>
+         <View style={styles.container}>
+            <Text style={FONTS.subheading}>Vegetarisch</Text>
+            <Text style={FONTS.caption}>Zeig mir nur vegetarische oder vegane Rezepte.</Text>
+         </View>
+        <Switch
+          trackColor={{ false: COLORS.primary, true: COLORS.secondary }}
+          thumbColor={isVegetarian ? 'green' : '#f4f3f4'}
+          onValueChange={() => setisVegetarian(prev => !prev)}
+          value={isVegetarian}
+        />
+      </View>
+
+      <View style={styles.switchContainer}>
+         <View style={styles.container}>
+            <Text style={FONTS.subheading}>Vegan</Text>
+            <Text style={FONTS.caption}>Zeig mir nur vegane Rezepte.</Text>
+         </View>
+        <Switch
+          trackColor={{ false: COLORS.primary, true: COLORS.secondary }}
+          thumbColor={isVegan ? 'green' : '#f4f3f4'}
+          onValueChange={() => setisVegan(prev => !prev)}
+          value={isVegan}
+        />
+      </View>
+
+      <View style={styles.switchContainer}>
+         <View style={styles.container}>
+            <Text style={FONTS.subheading}>Laktosefrei</Text>
+            <Text style={FONTS.caption}>Zeig mir nur laktosefreie Rezepte.</Text>
+         </View>
+        <Switch
+          trackColor={{ false: COLORS.primary, true: COLORS.secondary }}
+          thumbColor={isLactosefree ? 'green' : '#f4f3f4'}
+          onValueChange={() => setisLactosefree(prev => !prev)}
+          value={isLactosefree}
+        />
+      </View>
+
+      <View style={styles.switchContainer}>
+         <View style={styles.container}>
+            <Text style={FONTS.subheading}>Glutenfrei</Text>
+            <Text style={FONTS.caption}>Zeig mir nur glutenfreie Rezepte.</Text>
+         </View>
+        <Switch
+          trackColor={{ false: COLORS.primary, true: COLORS.secondary }}
+          thumbColor={isGlutenfree ? 'green' : '#f4f3f4'}
+          onValueChange={() => setisGlutenfree(prev => !prev)}
+          value={isGlutenfree}
+        />
+      </View>
+      <View style={styles.divider} />
+      <Text style={FONTS.subheading}>Freunde berücksichtigen:</Text>
+      <View style={styles.switchContainer}>
+         <View style={styles.container}>
+            <Text style={FONTS.subheading}>Robert Maier</Text>
+            <Text style={FONTS.caption}>Lebensmitteleinschränkungen von Robert miteinbeziehen</Text>
+         </View>
+        <Switch
+          trackColor={{ false: COLORS.primary, true: COLORS.secondary }}
+          thumbColor={user1 ? 'green' : '#f4f3f4'}
+          onValueChange={() => setuser1(prev => !prev)}
+          value={user1}
+        />
+      </View>
+      <View style={styles.switchContainer}>
+         <View style={styles.container}>
+            <Text style={FONTS.subheading}>Lisa Fuchs</Text>
+            <Text style={FONTS.caption}>Lebensmitteleinschränkungen von Lisa miteinbeziehen</Text>
+         </View>
+        <Switch
+          trackColor={{ false: COLORS.primary, true: COLORS.secondary }}
+          thumbColor={user2 ? 'green' : '#f4f3f4'}
+          onValueChange={() => setuser2(prev => !prev)}
+          value={user2}
+        />
+      </View>
+      <View style={styles.switchContainer}>
+         <View style={styles.container}>
+            <Text style={FONTS.subheading}>Sebastian Fuchs</Text>
+            <Text style={FONTS.caption}>Lebensmitteleinschränkungen von Sebastian miteinbeziehen</Text>
+         </View>
+        <Switch
+          trackColor={{ false: COLORS.primary, true: COLORS.secondary }}
+          thumbColor={user3 ? 'green' : '#f4f3f4'}
+          onValueChange={() => setuser3(prev => !prev)}
+          value={user3}
+        />
+      </View>
+  </View>
+    </ScrollView>
   );
-}
+};
+
+export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: COLORS.background,
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 24,
+  },
+  label: {
+    fontSize: 16,
+    color: COLORS.textPrimary,
+    marginBottom: 0,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: COLORS.border,
+    width: '100%',
+    marginVertical: 20,
+  },
+  headingContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 0,
+  },
+});
